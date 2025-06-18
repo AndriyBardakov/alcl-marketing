237de1fb05aaacd5098bfe1328c73dd4df215d2d
andriy-bardakov <andriy.bardakov@gmail.com>
2025-06-18 08:37:50 +0300

    headers update

--- src/components/dealership/Header.jsx
+++ src/components/dealership/Header.jsx
@@ -8,14 +8,14 @@ const Section3 = () => {
       className="dealership-hero bg-primary text-white text-center py-5"
       style={{
         backgroundImage:
-          "url(/images/source/5.png)",
+          "url(/images/source/orange_bg.png)",
         backgroundSize: "cover",
         padding: "5% 10%",
       }}
     >
       <div className="row">
         <div className="content-column col-sm-12 dealership-header">
-          <h1 className="text-center text-white">Dealership</h1>
+          <h1 className="text-center text-white fw-bolder" style={{fontSize: "48px"}}>Dealership</h1>
         </div>
       </div>
     </section>
--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -12,15 +12,15 @@ const index = () => {
       className="alcl-hero bg-primary text-white text-center py-5"
       style={{
         backgroundImage:
-          "url(/images/source/5.png)",
+          "url(/images/source/orange_bg.png)",
         backgroundSize: "cover",
         // marginTop: "150px",
         // height: "300px",
       }}
     >
       <div className="container d-flex flex-column justify-content-center align-items-center h-100">
-        <h1 className="mb-3">Mga Ka-Cheesemiss!</h1>
-        <h2>Welcome To The OFFICIAL ALCL MARKETING WEBSITE</h2>
+        <h1 className="mb-3 fw-bolder" style={{fontSize: "48px"}}>Mga Ka-Cheesemiss!</h1>
+        <h2 className="fw-bolder" style={{fontSize: "40px"}}>Welcome To The OFFICIAL ALCL MARKETING WEBSITE</h2>
         {/* <p className="lead text-white">Your trusted partner in digital solutions</p>
         <a href="#services" className="btn btn-light btn-lg">
           Explore Services
